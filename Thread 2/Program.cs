548fb798fc864686ee9229715c2b243dfb7245b1
Daniel <danielippolito9@gmail.com>
2023-04-26 16:54:38 +0200

    Esercizio 2

---
+++ Thread 2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Thread_2;
+class Program
+{
+
+    static void searchWord(string word, string path){
+        string[] lines = File.ReadAllLines("./" + path);
+        int count = 0;
+        foreach (string line in lines)
+        {
+            if (line.Contains(word))
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("La parola {0} è presente {1} volte nel file {2}", word, count, path);
+    } 
+    static void Main(string[] args)
+    {
+        Console.Write("Inserisci il nome del file da aprire, e le paole chiavi da cercare all'interno del file: ");
+        string[] words = Console.ReadLine().Split(' ');
+        string path = words[0];
+        words = words.Skip(1).ToArray();
+        
+        foreach (string word in words)
+        {
+            Thread t = new Thread(() => searchWord(word, path));
+            t.Start();
+        }
+
+        
+    }
+}
