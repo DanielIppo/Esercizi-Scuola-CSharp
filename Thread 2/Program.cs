68c93a3af952721c6905af5bcfedc8b185861932
Daniel <danielippolito9@gmail.com>
2023-05-04 18:43:08 +0200

    Migliorata l'efficienza

--- Thread 2/Program.cs
+++ Thread 2/Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace Thread_2;
+using System;
+using System.Threading;
 class Program
 {
 
-    static void searchWord(string word, string path){
-        string[] lines = File.ReadAllLines("./" + path);
+    static CountdownEvent countdown;
+    static void searchWord(string word, string[] lines, string fileName){
         int count = 0;
         foreach (string line in lines)
         {
@@ -16,22 +18,27 @@ class Program
                 }
             }
         }
-        Console.WriteLine("La parola {0} è presente {1} volte nel file {2}", word, count, path);
+        Console.WriteLine("La parola {0} è presente {1} volte nel file {2}", word, count, fileName);
+        countdown.Signal();
     } 
     static void Main(string[] args)
     {
         Console.Write("Inserisci il nome del file da aprire, e le paole chiavi da cercare all'interno del file: ");
         string[] words = Console.ReadLine().Split(' ');
         string path = words[0];
+        string[] lines = File.ReadAllLines("./" + path);
         words = words.Skip(1).ToArray();
+        int wordsLen = words.Length;
+        countdown = new CountdownEvent(wordsLen);
+
         
         foreach (string word in words)
         {
-            Thread t = new Thread(() => searchWord(word, path));
+            Thread t = new Thread(() => searchWord(word, lines, path));
             t.Start();
         }
 
-        Thread.Sleep(1000);
+        countdown.Wait();
 
         Console.WriteLine("Il programma è terminato");
     }
