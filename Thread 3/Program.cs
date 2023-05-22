54fb5762e56cfa55c6e3fb4bf87b3a07f4702143
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2023-05-22 08:37:40 +0000

    Iniziato Thread 3

---
+++ Thread 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Thread_3
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // Only get files that begin with the letter "c".
+            string[] dirs = Directory.GetFiles(args[0], "*.txt");
+            Console.WriteLine("The number of files starting with c is {0}.", dirs.Length);
+            foreach (string dir in dirs)
+            {
+                
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+    }
+}
